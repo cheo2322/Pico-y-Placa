c615d94a48affd2d29b90b3789e88c0eec7b1233
cheo2322 <36903744+cheo2322@users.noreply.github.com>
2019-09-05 16:21:37 -0500

    Update pico_y_placa.py

--- pico_y_placa.py
+++ pico_y_placa.py
@@ -37,14 +37,3 @@ def PicoYPlaca(number, date, time):
         return verify(last, wd, stToHour("7:00"), stToHour("9:30"), timeH) or verify(last, wd, stToHour("16:00"), stToHour("19:30"), timeH)
     else:
         return verify(last, wd, stToHour("5:00"), stToHour("20:00"), timeH)
-
-def main(number, date, time):
-    if PicoYPlaca(number, date, time):
-        print("Not allowed to circulate!")
-    else:
-        print("No problem :)")
-        
-main(2178, "2019,9,5", "12:30")        
-main(3478, "2019,9,12", "12:30")
-main(2170, "2019,9,6", "17:30")
-main(2170, "2019,9,7", "17:30")
