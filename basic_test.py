6a762dbb7ae1c79332f129651f69d88c2bc07d4d
cheo2322 <36903744+cheo2322@users.noreply.github.com>
2019-09-05 23:19:38 -0500

    Update basic_test.py

--- basic_test.py
+++ basic_test.py
@@ -5,14 +5,3 @@ assert(PicoYPlaca(3478, "2019,9,12", "12:30")==1)
 assert(PicoYPlaca(2170, "2019,9,6", "17:30")==1)
 assert(PicoYPlaca(2170, "2019,9,7", "17:30")==0)
 assert(PicoYPlaca(134, "1998,6,22", "4:20")==0)
-
-def main(number, date, time):
-    if PicoYPlaca(number, date, time):
-        print("Not allowed to circulate!")
-    else:
-        print("No problem :)")
-
-main(2178, "2019,9,5", "12:30")        
-main(3478, "2019,9,12", "12:30")
-main(2170, "2019,9,6", "17:30")
-main(2170, "2019,9,7", "17:30")
